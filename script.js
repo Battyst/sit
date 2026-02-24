8f9ba3e7d299eaa82b53899d47ea059be64ffff1
Battyst <samuelbatistam3@gmail.com>
2026-02-24 07:52:29 -0300

    atulizando o site

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-const btn = document.getElementById('btn-entrar');
-const intro = document.getElementById('intro');
-const content = document.getElementById('main-content');
-
-btn.addEventListener('click', () => {
-
-    intro.style.opacity = '0';
-    intro.style.transform = 'scale(1.1)';
-
-    setTimeout(() => {
-        intro.style.display = 'none';
-        content.classList.remove('content-hidden');
-        content.classList.add('content-visible');
-    }, 800);
-});
+document.getElementById('btn-scroll').onclick = function() {
+document.getElementById('galeria').scrollIntoView({ behavior: 'smooth' });
+};
